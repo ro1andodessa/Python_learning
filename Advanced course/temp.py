08761afd5dd49aadb7cd8988a9834fcfcc4f4663
Nick Ryzhkov <nikolay.citrus@gmail.com>
2024-09-23 12:12:13 +0300

    Added some task in 6.3

--- Advanced course/temp.py
+++ Advanced course/temp.py
@@ -1,3 +1,12 @@
-l = [['a'], ['b'], ['c']]
+s = '1 1 2 2 5 5 5 5 6 7 8'
+numbers = [int(i) for i in s.split()]
+numbers_set = set()
+print(numbers)
+for i in numbers:
+    if i in numbers_set:
+        print('YES')
+    else:
+        print('NO')
+    numbers_set.add(i)
 
-print(l[0:1])
+print(numbers_set)
